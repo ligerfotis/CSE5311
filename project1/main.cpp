e41cc254b5826ce93b6cb9bd708a2793ccec531a
ligerfotis <ligerfotis@gmail.com>
2020-02-22 13:23:07 -0600

    add lcs array

--- project1/main.cpp
+++ project1/main.cpp
@@ -11,35 +11,74 @@ using namespace std;
 int max(int a, int b);
 
 /* Returns length of LCS for X[0..m-1], Y[0..n-1] */
-int lcs(int* arr1, int* arr2, int n, int m)
+int lcs( int *X, int *Y, int m, int n )
 {
-    /*1.Use Theta(mn) space*/
     int L[m + 1][n + 1];
     int i, j;
 
-    /* Following steps build L[m+1][n+1] in bottom up fashion. Note
-      that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= m; j++) {
-            if (i == 0 || j == 0)
-                L[i][j] = 0;
+    /* Following steps build L[m+1][n+1] in
+       bottom up fashion. Note that L[i][j]
+       contains length of LCS of X[0..i-1]
+       and Y[0..j-1] */
+    for (i = 0; i <= m; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+        if (i == 0 || j == 0)
+            L[i][j] = 0;
 
-            else if (arr1[i - 1] == arr2[j - 1])
-                L[i][j] = L[i - 1][j - 1] + 1;
+        else if (X[i - 1] == Y[j - 1])
+            L[i][j] = L[i - 1][j - 1] + 1;
 
-            else
-                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+        else
+            L[i][j] = max(L[i - 1][j], L[i][j - 1]);
         }
     }
+    // Following code is used to print LCS
+    int index = L[m][n];
 
-    /* L[m][n] contains length of LCS for X[0..n-1] and Y[0..m-1] */
+    // Create a character array to store the lcs string
+    int lcs[index+1];
+    lcs[index] = '\0'; // Set the terminating character
+
+    // Start from the right-most-bottom-most corner and
+    // one by one store characters in lcs[]
+    i = m;
+    j = n;
+    while (i > 0 && j > 0)
+    {
+      // If current character in X[] and Y are same, then
+      // current character is part of LCS
+      if (X[i-1] == Y[j-1])
+      {
+          lcs[index-1] = X[i-1]; // Put current character in result
+          i--; j--; index--;     // reduce values of i, j and index
+      }
+
+      // If not same, then find the larger of two and
+      // go in the direction of larger value
+      else if (L[i-1][j] > L[i][j-1])
+         i--;
+      else
+         j--;
+    }
+
+    // Print the lcs
+    cout << "LCS is ";
+    for (int i =0; i< L[m][n]; i++){
+    cout << lcs[i];
+    }
+    cout << "\n";
+
+    /* L[m][n] contains length of LCS
+    for X[0..n-1] and Y[0..m-1] */
     return L[m][n];
 }
 
 /* Utility function to get max of 2 integers */
 int max(int a, int b)
 {
-    return (a > b) ? a : b;
+    return (a > b)? a : b;
 }
 
 /* Driver program to test above function */
