6f7389e21cfd84d2944b7c9741ee29d905dce9ce
ligerfotis <ligerfotis@gmail.com>
2020-02-28 14:28:40 -0600

    fixed second way for LCS

--- project1/main.cpp
+++ project1/main.cpp
@@ -31,11 +31,10 @@ vector<int> replaceSeq(int *X, vector<vector<int> > positions, int m){
 }
 
 // Utility function to print LIS
-void printVector(vector<int>& arr)
+void printVector(vector<int> arr)
 {
     for (int x : arr)
         cout << x << " ";
-    cout << endl;
 }
 
 /*determine the positions where the symbol appears in the second sequence*/
@@ -73,11 +72,11 @@ inline bool operator<(const Node& lhs, const Node& rhs)
 }
 
 // Function to print LIS using parent array
-void print(vector<int> input, map<int,int> parent, set<Node> S)
+vector <int>  getLSIS(vector<int> input, map<int,int> parent, set<Node> S)
 {
 	// container to store LIS in reverse order
 	stack<int> lis;
-
+    vector <int> LSISseq;
 	// start from the last element of S
 	int index = S.rbegin()->index;
 
@@ -92,16 +91,18 @@ void print(vector<int> input, map<int,int> parent, set<Node> S)
 	}
 
 	// print LIS
-	cout << "LIS is ";
+	//cout << "LIS is ";
 	while(!lis.empty())
 	{
-		cout << lis.top() << " ";
+		//cout << lis.top() << " ";
+		LSISseq.push_back(lis.top());
 		lis.pop();
 	}
+	return LSISseq;
 }
 
 // Function to find Longest Increasing Subsequence in given array
-void printLIS(vector<int> arr, int n)
+vector <int> LSIS(vector<int> arr, int n)
 {
 	// create an empty ordered set S (ith element in S is defined as the
 	// smallest integer that ends an increasing sequence of length i)
@@ -136,13 +137,21 @@ void printLIS(vector<int> arr, int n)
 	}
 
 	// print LIS using parent map
-	print(arr, parent, S);
+	vector <int> LSISseq = getLSIS(arr, parent, S);
+	return LSISseq;
 }
-/* Dynamic Programming C++ implementation of LCS problem */
-int max(int a, int b);
+vector<int> getLCSFromIndexes(vector<int> LSISseq, int *Y){
+
+    vector<int> lcs2;
+
+    for (int i = 0; i < (int)LSISseq.size(); i++)
+        lcs2.push_back(Y[LSISseq[i]]);
+
+    return lcs2;
+}
 
 /* Returns length of LCS for X[0..m-1], Y[0..n-1] */
-int lcs( int *X, int *Y, int m, int n )
+vector<int> lcs( int *X, int *Y, int m, int n )
 {
     //int L[m + 1][n + 1];
     // Create a vector containing n
@@ -167,10 +176,34 @@ int lcs( int *X, int *Y, int m, int n )
             L[i][j] = max(L[i - 1][j], L[i][j - 1]);
         }
     }
+    // Following code is used to print LCS
+   int index = L[m][n];
 
-    /* L[m][n] contains length of LCS
-    for X[0..n-1] and Y[0..m-1] */
-    return L[m][n];
+   // Create a vector to store the lcs string
+   vector<int>  lcsSeq( index, -1); // Set the terminating character
+
+   // Start from the right-most-bottom-most corner and
+   // one by one store characters in lcs[]
+   i = m, j = n;
+   while (i > 0 && j > 0)
+   {
+      // If current character in X[] and Y are same, then
+      // current character is part of LCS
+      if (X[i-1] == Y[j-1])
+      {
+          lcsSeq[index-1] = X[i-1]; // Put current character in result
+          i--; j--; index--;     // reduce values of i, j and index
+      }
+
+      // If not same, then find the larger of two and
+      // go in the direction of larger value
+      else if (L[i-1][j] > L[i][j-1])
+         i--;
+      else
+         j--;
+   }
+
+    return lcsSeq;
 }
 /* Utility function to get max of 2 integers */
 int max(int a, int b)
@@ -207,21 +240,15 @@ int main()
         Matrix Method
     */
     float time_taken;
-    /*
+
     start_time = clock();
-    printf("Matrix LCS length: %d ", lcs(arr1, arr2, n, m));
+    vector <int> lcs1 = lcs(arr1, arr2, n, m);
+    printf("Matrix LCS length: %d ", (int)lcs1.size());
     end_time = clock();
     time_taken = float(end_time - start_time) / float(CLOCKS_PER_SEC);
     cout << "CPU : " << fixed
          << time_taken << setprecision(3);
     cout << " sec " << endl;
-*/
-//    cout << "\n";
-//    for (int i = 0; i < n; i++)
-//    {
-//        cout << arr1[i] << " ";
-//    }
-//    cout << "\n";
 
     /* 1.b.1
     For each of the 256 alphabet symbols, determine the positions (descending order)
@@ -241,26 +268,33 @@ int main()
     vector<int> intermediate = replaceSeq(arr1, pos, m);
 
     int k = intermediate.size();
-    cout << "LSISinputLength: " << k;
-    end_time = clock();
-    time_taken = float(end_time - start_time) / float(CLOCKS_PER_SEC);
-    cout << " CPU : " << fixed
-         << time_taken << setprecision(3);
-    cout << " sec " << endl;
+    cout << "LSISinputLength: " << k<< endl;
+
     /*1.b.3
     Compute a LSIS of the intermediate sequence
     https://www.techiedelight.com/longest-increasing-subsequence/
     */
-    printLIS(intermediate, k);
+    cout << "LSIS length: ";
+    vector<int> LSISseq = LSIS(intermediate, k);
+    cout << LSISseq.size();
     //cout << "LSIS LCS length: " << printLIS(intermediate);
     end_time = clock();
     time_taken = float(end_time - start_time) / float(CLOCKS_PER_SEC);
     cout << " CPU : " << fixed
          << time_taken << setprecision(3);
     cout << " sec " << endl;
-    // Print the lcs
-    //lcs(arr1, arr2, n, m);
 
+    /*1.b.4
+    The sequence of values from the LSIS may be used
+    as indexes to the second sequence to obtain an LCS.
+    */
+    vector<int> lcs2  = getLCSFromIndexes(LSISseq, arr2);
+    bool identical = std::equal(lcs1.begin(), lcs1.end(), lcs2.begin());
+    if (identical)
+        printVector(lcs1);
+    cout << -1 << endl;
+
+    //printVector(lcs2);
 
     return 0;
 }
