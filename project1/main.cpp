b4d72361e1446efc5f63b274449beb849ae6f691
ligerfotis <ligerfotis@gmail.com>
2020-02-27 20:15:35 -0600

    fix LSIS

--- project1/main.cpp
+++ project1/main.cpp
@@ -5,6 +5,10 @@
 #include <list>
 #include <vector>
 #include <iterator>
+#include <ctime>
+#include <stack>
+#include <set>
+#include <map>
 #define ALPHABET_SIZE 256
 
 using namespace std;
@@ -55,71 +59,96 @@ std::vector<std::vector<int>> countSort(int *Y, int m)
     return positions;
 }
 
-/* Dynamic Programming C++ implementation of LIS problem */
-
-
-// Binary search
-int GetCeilIndex(std::vector<int> arr, vector<int>& T, int l, int r,
-                 int key)
+// Data structure to store an element and its index in the array
+struct Node
 {
-    while (r - l > 1) {
-        int m = l + (r - l) / 2;
-        if (arr[T[m]] >= key)
-            r = m;
-        else
-            l = m;
-    }
+	int elem;
+	int index;
+};
 
-    return r;
+// overload compare operator for inserting into set
+inline bool operator<(const Node& lhs, const Node& rhs)
+{
+	return lhs.elem < rhs.elem;
 }
 
-int LSIS(std::vector<int> arr, int n)
+// Function to print LIS using parent array
+void print(vector<int> input, map<int,int> parent, set<Node> S)
 {
-    // Add boundary case, when array n is zero
-    // Depend on smart pointers
+	// container to store LIS in reverse order
+	stack<int> lis;
 
-    vector<int> tailIndices(n, 0); // Initialized with 0
-    vector<int> prevIndices(n, -1); // initialized with -1
+	// start from the last element of S
+	int index = S.rbegin()->index;
 
-    int len = 1; // it will always point to empty location
-    for (int i = 1; i < n; i++) {
-        if (arr[i] < arr[tailIndices[0]]) {
-            // new smallest value
-            tailIndices[0] = i;
-        }
-        else if (arr[i] > arr[tailIndices[len - 1]]) {
-            // arr[i] wants to extend largest subsequence
-            prevIndices[i] = tailIndices[len - 1];
-            tailIndices[len++] = i;
-        }
-        else {
-            // arr[i] wants to be a potential condidate of
-            // future subsequence
-            // It will replace ceil value in tailIndices
-            int pos = GetCeilIndex(arr, tailIndices, -1,
-                                   len - 1, arr[i]);
+	// get length of LIS
+	int n = S.size();
 
-            prevIndices[i] = tailIndices[pos - 1];
-            tailIndices[pos] = i;
-        }
-    }
+	// retrieve LIS from parent array
+	while (n--)
+	{
+		lis.push(input[index]);
+		index = parent[index];
+	}
 
-    vector<int> output;
-
-    for (int i = tailIndices[len - 1]; i >= 0; i = prevIndices[i])
-        output.push_back(arr[i]);
-
-    sort(output.begin(), output.end());
-    printVector(output);
-
-    return len;
+	// print LIS
+	cout << "LIS is ";
+	while(!lis.empty())
+	{
+		cout << lis.top() << " ";
+		lis.pop();
+	}
 }
+
+// Function to find Longest Increasing Subsequence in given array
+void printLIS(vector<int> arr, int n)
+{
+	// create an empty ordered set S (ith element in S is defined as the
+	// smallest integer that ends an increasing sequence of length i)
+	set<Node> S;
+
+	// parent[i] will store the predecessor of element with index i in the
+	// LIS ending at element with index i
+	map<int, int> parent;
+
+	// process every element one by one
+	for (int i = 0; i < n; i++)
+	{
+		// construct node from current element and its index
+		Node curr = {arr[i], i};
+
+		// ignore the current element if already present in the set
+		if (S.find(curr) != S.end()) {
+			continue;
+		}
+
+		// insert current node into the set and get iterator to the
+		// inserted node
+		auto it = S.insert(curr).first;
+
+		// if the node is not inserted at the end, then delete the next node
+		if (++it != S.end())
+			S.erase(it);
+
+		// get iterator to the current node and update parent
+		it = S.find(curr);
+		parent[i] = (--it)->index;
+	}
+
+	// print LIS using parent map
+	print(arr, parent, S);
+}
+/* Dynamic Programming C++ implementation of LCS problem */
+int max(int a, int b);
+
 /* Returns length of LCS for X[0..m-1], Y[0..n-1] */
 int lcs( int *X, int *Y, int m, int n )
 {
-    int L[m + 1][n + 1];
+    //int L[m + 1][n + 1];
+    // Create a vector containing n
+    //vectors of size m.
+    vector<vector<int> > L( n+1, vector<int> (m+1, 0));
     int i, j;
-
     /* Following steps build L[m+1][n+1] in
        bottom up fashion. Note that L[i][j]
        contains length of LCS of X[0..i-1]
@@ -138,64 +167,29 @@ int lcs( int *X, int *Y, int m, int n )
             L[i][j] = max(L[i - 1][j], L[i][j - 1]);
         }
     }
-    // Following code is used to print LCS and store positions where the symbol appears in the second sequence
-    int index = L[m][n];
-
-    // Create a integer array to store the lcs string
-    int lcs[index+1];
-    lcs[index] = '\0'; // Set the terminating character
-
-    // Start from the right-most-bottom-most corner and
-    // one by one store characters in lcs[]
-    i = m;
-    j = n;
-    while (i > 0 && j > 0)
-    {
-      // If current character in X[] and Y are same, then
-      // current character is part of LCS
-      if (X[i-1] == Y[j-1])
-      {
-          lcs[index-1] = X[i-1]; // Put current character in result
-                                //store the position of the character
-          i--; j--; index--;     // reduce values of i, j and index
-      }
-
-      // If not same, then find the larger of two and
-      // go in the direction of larger value
-      else if (L[i-1][j] > L[i][j-1])
-         i--;
-      else
-         j--;
-    }
-
-    // Print the lcs
-    cout << "Longest Common Subsequence\n";
-    for (int i =0; i< L[m][n]; i++){
-    cout << lcs[i] << " ";
-    }
-    cout << "\n";
 
     /* L[m][n] contains length of LCS
     for X[0..n-1] and Y[0..m-1] */
     return L[m][n];
 }
-
 /* Utility function to get max of 2 integers */
 int max(int a, int b)
 {
     return (a > b)? a : b;
 }
-
 /* Driver program to test above function */
 int main()
 {
-    char filename[]="lab1.d.dat";
+    /* Time function returns the time since the
+        Epoch(jan 1 1970). Returned time is in seconds. */
+    clock_t start_time, end_time;
+    char filename[]="lab1.a.dat";
     int n,m,indicator;
 
     ifstream File;
     File.open(filename);
     File >> n >> m;
-    cout << "Size of array: "<< n;
+    cout << "Size of array: "<< n << endl;
     int arr1[n];
     int arr2[m];
     //assess m=n
@@ -209,52 +203,64 @@ int main()
         File >> arr2[i];
     }
     File.close();
-    cout << "\n";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr1[i] << " ";
-    }
-    cout << "\n";
+    /* 1.a
+        Matrix Method
+    */
+    float time_taken;
+    /*
+    start_time = clock();
+    printf("Matrix LCS length: %d ", lcs(arr1, arr2, n, m));
+    end_time = clock();
+    time_taken = float(end_time - start_time) / float(CLOCKS_PER_SEC);
+    cout << "CPU : " << fixed
+         << time_taken << setprecision(3);
+    cout << " sec " << endl;
+*/
+//    cout << "\n";
+//    for (int i = 0; i < n; i++)
+//    {
+//        cout << arr1[i] << " ";
+//    }
+//    cout << "\n";
 
     /* 1.b.1
     For each of the 256 alphabet symbols, determine the positions (descending order)
     where the symbol appears in the second sequence.
     Do not do 256 passes over the second sequence!(Think about counting sort)
     */
+    start_time = clock();
     //Complexity O(2*AlhabetSize + m)
     vector< vector<int> > pos = countSort(arr2, m);
-    // Displaying the 2D vector
-        // Store count of each character
-    for (int i = 0; i < ALPHABET_SIZE; i++){
-        if (pos[i].size() != 0){
-            cout  << i << ": " ;
-            for (int j = 0; j < (int)pos[i].size(); j++)
-                cout << pos[i][j] << " ";
-            cout << endl;
-        }
-    }
+
     /* 1.b.2
     Produce an intermediate sequence by replacingeach symbol in the first
     sequence by its positions from the second sequence
     */
+
     // Complexity O(mlogm)
     vector<int> intermediate = replaceSeq(arr1, pos, m);
-    // Displaying the intermediate vector
-    cout << "Intermediate Sequence\n";
-    for (int i = 0; i < (int)intermediate.size(); i++)
-         cout << intermediate[i] << " ";
-    cout << "\n";
+
+    int k = intermediate.size();
+    cout << "LSISinputLength: " << k;
+    end_time = clock();
+    time_taken = float(end_time - start_time) / float(CLOCKS_PER_SEC);
+    cout << " CPU : " << fixed
+         << time_taken << setprecision(3);
+    cout << " sec " << endl;
     /*1.b.3
     Compute a LSIS of the intermediate sequence
+    https://www.techiedelight.com/longest-increasing-subsequence/
     */
-    int k = intermediate.size();
-
-    cout << "Longest Strictly Increasing Subsequence\n";
-    // Complexity O()
-    LSIS(intermediate, k );
+    printLIS(intermediate, k);
+    //cout << "LSIS LCS length: " << printLIS(intermediate);
+    end_time = clock();
+    time_taken = float(end_time - start_time) / float(CLOCKS_PER_SEC);
+    cout << " CPU : " << fixed
+         << time_taken << setprecision(3);
+    cout << " sec " << endl;
     // Print the lcs
     //lcs(arr1, arr2, n, m);
-    //printf("Length of LCS is %d\n", lcs(arr1, arr2, n, m));
+
 
     return 0;
 }
