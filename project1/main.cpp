f324339da0bfe7861814172b8fabfecd50a763a9
ligerfotis <ligerfotis@gmail.com>
2020-02-29 19:08:29 -0600

    completed

--- project1/main.cpp
+++ project1/main.cpp
@@ -9,6 +9,7 @@
 #include <stack>
 #include <set>
 #include <map>
+#include <math.h>
 #define ALPHABET_SIZE 256
 
 using namespace std;
@@ -64,87 +65,62 @@ std::vector<std::vector<int>> countSort(int *Y, int m)
     return positions;
 }
 
-// Data structure to store an element and its index in the array
-struct Node
-{
-	int elem;
-	int index;
-};
+/*
+    Function to find Longest Increasing Subsequence in given array
+    https://en.wikipedia.org/wiki/Longest_increasing_subsequence
+*/
+vector <int> LSIS(vector<int> arr, int n){
+    vector<int> pred(n,-1);
 
-// overload compare operator for inserting into set
-inline bool operator<(const Node& lhs, const Node& rhs)
-{
-	return lhs.elem < rhs.elem;
-}
+    vector<int> tail(n+1, -1);
 
-// Function to print LIS using parent array
-vector <int>  getLSIS(vector<int> input, map<int,int> parent, set<Node> S)
-{
-	// container to store LIS in reverse order
-	stack<int> lis;
-    vector <int> LSISseq;
-	// start from the last element of S
-	int index = S.rbegin()->index;
+    int length = 0;
+    for (int i = 0; i < n-1; i++){
+        // Binary search for the largest positive j ≤ L
+        // such that X[M[j]] <= X[i]
+        int lo = 1;
+        int hi = length;
 
-	// get length of LIS
-	int n = S.size();
+        while (lo <=hi){
+            int mid = ceil( (lo + hi)/2 );
+            if ( arr[ tail[ mid] ] < arr[ i] )
+                lo = mid+1;
+            else
+                hi = mid-1;
+        }
+        // After searching, lo is 1 greater than the
+        // length of the longest prefix of X[i]
+        int newL = lo;
 
-	// retrieve LIS from parent array
-	while (n--)
-	{
-		lis.push(input[index]);
-		index = parent[index];
-	}
+        // The predecessor of X[i] is the last index of
+        // the subsequence of length newL-1
+        pred[i] = tail[newL - 1];
+        tail[newL] = i;
 
-	// print LIS
-	//cout << "LIS is ";
-	while(!lis.empty())
+        if (newL > length)
+            // If we found a subsequence longer than any we've
+            // found yet, update L
+            length = newL;
+    }
+    // Reconstruct the longest increasing subsequence
+    vector<int> seq;
+    stack<int> s;
+
+    int k = tail[length];
+    // fill as tack to reverse the sequence
+    for (int i = 0; i < length; i++){
+        s.push(arr[k]);
+        k = pred[k];
+    }
+    // the orders sequence
+    while(!s.empty())
 	{
 		//cout << lis.top() << " ";
-		LSISseq.push_back(lis.top());
-		lis.pop();
+		seq.push_back(s.top());
+		s.pop();
 	}
-	return LSISseq;
-}
+    return seq;
 
-// Function to find Longest Increasing Subsequence in given array
-vector <int> LSIS(vector<int> arr, int n)
-{
-	// create an empty ordered set S (ith element in S is defined as the
-	// smallest integer that ends an increasing sequence of length i)
-	set<Node> S;
-
-	// parent[i] will store the predecessor of element with index i in the
-	// LIS ending at element with index i
-	map<int, int> parent;
-
-	// process every element one by one
-	for (int i = 0; i < n; i++)
-	{
-		// construct node from current element and its index
-		Node curr = {arr[i], i};
-
-		// ignore the current element if already present in the set
-		if (S.find(curr) != S.end()) {
-			continue;
-		}
-
-		// insert current node into the set and get iterator to the
-		// inserted node
-		auto it = S.insert(curr).first;
-
-		// if the node is not inserted at the end, then delete the next node
-		if (++it != S.end())
-			S.erase(it);
-
-		// get iterator to the current node and update parent
-		it = S.find(curr);
-		parent[i] = (--it)->index;
-	}
-
-	// print LIS using parent map
-	vector <int> LSISseq = getLSIS(arr, parent, S);
-	return LSISseq;
 }
 vector<int> getLCSFromIndexes(vector<int> LSISseq, int *Y){
 
@@ -155,6 +131,7 @@ vector<int> getLCSFromIndexes(vector<int> LSISseq, int *Y){
 
     return lcs2;
 }
+
 
 /* Returns length of LCS for X[0..m-1], Y[0..n-1] */
 vector<int> lcs( int *X, int *Y, int m, int n )
@@ -222,7 +199,7 @@ int main(int argc, char** argv)
     /* Time function returns the time since the
         Epoch(jan 1 1970). Returned time is in seconds. */
     clock_t start_time, end_time;
-    char filename[]="lab1.c.dat";
+    char filename[]="lab1.e.dat";
     ifstream File;
     if (argc > 1){
       cout << "Filename: " << argv[1] << endl;
@@ -257,7 +234,7 @@ int main(int argc, char** argv)
 
     start_time = clock();
     vector <int> lcs1 = lcs(arr1, arr2, n, m);
-    printf("Matrix LCS length: %d ", (int)lcs1.size());
+    cout << "Matrix LCS length:" << (int)lcs1.size();
     end_time = clock();
     time_matrix_LCS = float(end_time - start_time) / float(CLOCKS_PER_SEC);
 //    cout << "CPU : " << fixed
@@ -289,7 +266,9 @@ int main(int argc, char** argv)
     https://www.techiedelight.com/longest-increasing-subsequence/
     */
     vector<int> LSISseq = LSIS(intermediate, k);
-    //cout << LSISseq.size() << endl;
+    //printVector(LSISseq);
+    //int len = LSIS(intermediate, k);
+    //cout << "LSIS length: " << len << endl;
     //cout << "LSIS LCS length: " << printLIS(intermediate);
     end_time = clock();
     time_LSIS_LCS = float(end_time - start_time) / float(CLOCKS_PER_SEC);
